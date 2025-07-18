c37a2e8dbf8c397c7a0f152330a64f1bf6a9ecb3
Safmica <safmikasa@gmail.com>
2025-07-19 01:59:20 +0700

    Feat: enhance navbar link styles with transition effects

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -5,22 +5,22 @@ export function Navbar() {
         <h1 className="text-xl font-bold">M. Safarudin P</h1>
         <ul className="flex gap-6 text-md font-bold">
           <li>
-            <a href="#home" className="hover:text-gray-400">
+            <a href="#home"   className="relative transition-colors duration-300 before:content-[''] before:absolute before:bottom-0 before:left-1/2 before:w-0 before:h-0.5 before:bg-white before:transition-all before:duration-300 hover:before:w-full hover:before:left-0">
               Home
             </a>
           </li>
           <li>
-            <a href="#about" className="hover:text-gray-400">
+            <a href="#about" className="relative transition-colors duration-300 before:content-[''] before:absolute before:bottom-0 before:left-1/2 before:w-0 before:h-0.5 before:bg-white before:transition-all before:duration-300 hover:before:w-full hover:before:left-0">
               About
             </a>
           </li>
           <li>
-            <a href="#projects" className="hover:text-gray-400">
+            <a href="#projects" className="relative transition-colors duration-300 before:content-[''] before:absolute before:bottom-0 before:left-1/2 before:w-0 before:h-0.5 before:bg-white before:transition-all before:duration-300 hover:before:w-full hover:before:left-0">
               Projects
             </a>
           </li>
           <li>
-            <a href="#contact" className="hover:text-gray-400">
+            <a href="#contact" className="relative transition-colors duration-300 before:content-[''] before:absolute before:bottom-0 before:left-1/2 before:w-0 before:h-0.5 before:bg-white before:transition-all before:duration-300 hover:before:w-full hover:before:left-0">
               Contact
             </a>
           </li>
